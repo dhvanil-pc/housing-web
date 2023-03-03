195cb24ebcb7a1e397a30819cb42be622bad3dbd
Dhvanil <jignasha.makwna@alpsinfo.net>
2023-03-03 11:45:31 +0530

    initilizzed

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PropertyCardComponent } from './Property/property-card/property-card.component';
+import { PropertyListComponent } from './Property/property-card/property-list/property-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+      NavBarComponent,
+      PropertyCardComponent,
+      PropertyListComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
